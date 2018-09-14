60e014a956331e1b4d34df04be9600f86398eb14
strdr4605 <str.dr4605@gmail.com>
2018-09-14 21:06:18 +0300

    simple progress bar

---
+++ assets/scripts.js
@@ -0,0 +1,17 @@
+function progress(beginningTime, endingTime, curentTime, $element) {
+  let curentBarWidth = curentTime - beginningTime;
+  let totalBarWidth = endingTime - beginningTime;
+  let progressBarWidth = curentBarWidth * $element.width() / totalBarWidth;
+  $element.find('div').animate({ width: progressBarWidth }, 500).html(Math.floor(curentTime / 60) + ":" + curentTime % 60);
+  if(curentTime <= endingTime) {
+      setTimeout(function() {
+          progress(beginningTime, endingTime, curentTime + 1, $element);
+      }, 1000); // Fix for minutes not seconds -> 60000
+  }
+};
+
+let beginningTime = 7 * 60;
+let endingTime = 21 * 60;
+let curentTime = new Date().getHours() * 60 + new Date().getMinutes();
+
+progress(beginningTime, endingTime, curentTime, $('#progressBar'));
