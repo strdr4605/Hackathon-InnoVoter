6b07d62fb5624aeee8664cfa93cfd755b5a8822f
strdr4605 <str.dr4605@gmail.com>
2018-09-15 15:38:45 +0300

    mvp chart

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,37 +1,69 @@
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
+let ctx = document.getElementById("myChart").getContext('2d');
+let hours = Math.floor((curentTime - beginningTime) / 60) + 1;
+
+function chartValues(hours) {
+  let dataArray = [];
+  for (let i = 0; i < hours; i++) {
+    dataArray.push(Math.floor(Math.random() * 10000));
+  }
+  return dataArray;
+}
+
+let myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [
+      "7:00", 
+      "8:00", 
+      "9:00", 
+      "10:00", 
+      "11:00", 
+      "12:00",
+      "13:00", 
+      "14:00", 
+      "15:00", 
+      "16:00", 
+      "17:00", 
+      "18:00",
+      "19:00", 
+      "20:00", 
+      "21:00"
+    ],
+    datasets: [{
+      label: 'Numarul de Votanti',
+      data: chartValues(hours),
+      backgroundColor: [
+        'rgba(88,137,192,0.38)'
+      ],
+      borderColor: [
+        'rgba(88,137,192,1)'
+      ],
+      borderWidth: 2
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          fontColor: "#507fb3",
+          fontSize: 20,
+          beginAtZero:true
         }
+      }],
+      xAxes: [{
+        ticks: {
+            fontColor: "#507fb3",
+            fontSize: 20,
+            beginAtZero: true
+        }
+    }]
+    },
+    legend: {
+      labels: {
+        // This more specific font property overrides the global property
+        fontColor: '#507fb3',
+        fontSize: 30,
+      }
     }
+  }
 });
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,15 @@
 function progress(beginningTime, endingTime, curentTime, $element) {
   let curentBarWidth = curentTime - beginningTime;
   let totalBarWidth = endingTime - beginningTime;
-  let progressBarWidth = curentBarWidth * $element.width() / totalBarWidth;
-  $element.find('.progress-bar').animate({ width: progressBarWidth }, 500).html(Math.floor(curentTime / 60) + ":" + curentTime % 60);
+  let progressBarWidth = curentBarWidth * $element.width() / totalBarWidth - 25; // 2 is bubble size
+  let timeText = `<p>${Math.floor(curentTime / 60)}:${(curentTime % 60)}</p>`
+  $element.find('.progress-bar').animate({ width: progressBarWidth }, 500).html(timeText);
   if(curentTime <= endingTime) {
       setTimeout(function() {
           progress(beginningTime, endingTime, curentTime + 1, $element);
-      }, 1000); // Fix for minutes not seconds -> 60000
+      }, 60000); // Fix for minutes not seconds -> 60000
   }
+  return progressBarWidth;
 };
 
 let beginningTime = 7 * 60;
@@ -15,3 +17,12 @@ let endingTime = 21 * 60;
 let curentTime = new Date().getHours() * 60 + new Date().getMinutes();
 
 progress(beginningTime, endingTime, curentTime, $('.progress'));
+
+// buttons actions
+
+$('#dropdown').click(() => {
+  $('#chart').toggleClass('close-chart');
+  $('#hours').toggleClass('hide');
+  $('.progress-bar p').toggleClass('hide');
+  $('#dropdown').toggleClass('rotate');
+});
